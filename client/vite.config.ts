dd93cd98f14b4a9ba011cfd7ca481227a61ce399
Pavel <kolbun@evvve.net>
2026-07-07 17:50:42 +0300

    Alias for the protocol .

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "node:path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@protocol": path.resolve(
+        __dirname,
+        "../server/src/protocol/protocol.ts"
+      ),
+    },
+  },
 })
