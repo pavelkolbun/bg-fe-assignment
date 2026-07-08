0a3703db6257143b6ee233a7434b55cc60e93205
pavelkolbun <p.m.kolbun@gmail.com>
2026-07-08 03:49:48 +0300

    Add ConnectionStatusBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import {StoreProvider} from "./state/context.tsx";
 import {BetsTable} from "./components/BetsTable/BetsTable.tsx";
 import {MultiplierTicker} from "./components/MultiplierTicker/MultiplierTicker.tsx";
+import {ConnectionStatusBar} from "./components/ConnectionStatusBar/ConnectionStatusBar.tsx";
 
 export function App(): React.JSX.Element {
   return (
     <StoreProvider>
       <div className="app">
+        <ConnectionStatusBar />
         <main className="app-main">
           <aside className="app-sidebar">
             <MultiplierTicker />
