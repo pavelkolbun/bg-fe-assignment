2651e83052ebbead6007460baa058466225b4a0f
pavelkolbun <p.m.kolbun@gmail.com>
2026-07-08 03:56:45 +0300

    Missed App changes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import {StoreProvider} from "./state/context.tsx";
 import {BetsTable} from "./components/BetsTable/BetsTable.tsx";
 import {MultiplierTicker} from "./components/MultiplierTicker/MultiplierTicker.tsx";
 import {ConnectionStatusBar} from "./components/ConnectionStatusBar/ConnectionStatusBar.tsx";
+import {BetPanel} from "./components/BetPanel/BetPanel.tsx";
 
 export function App(): React.JSX.Element {
   return (
@@ -11,6 +12,7 @@ export function App(): React.JSX.Element {
         <main className="app-main">
           <aside className="app-sidebar">
             <MultiplierTicker />
+            <BetPanel />
           </aside>
           <BetsTable />
         </main>
